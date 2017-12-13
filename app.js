de21e19e81715168157692bc87418bb37b88376f
Johnbayronp <webdeprueba1.1@gmail.com>
2017-12-12 21:54:37 -0500

    conectando mongoose
    aprendiendo a conexion con mongodb

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 var express = require("express");
 var bodyparser = require("body-parser");
 var app = express();
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost/fotos');
+
+var userSchemaJSON = {
+   email:String,
+   password:String,
+};
+
+var user_schema = new Schema(userSchemaJSON); // crea un objeto 
+
+var User = mongoose.model('User', user_schema);
 
 app.use("/archivos", express.static('public')); // para montar un middleware, static() retorna el middle.. 
 app.use(express.static('assets'));
@@ -17,14 +30,24 @@ app.get("/", function(req,res){
 
 
 app.get("/login",function(req,res){	
+
+	User.find(function(err,doc){
+		console.log(doc);
 	res.render("login");
+	});
+
 });
 
 
 app.post("/users" , function(req,res){
-	console.log("Email: "+ req.body.email +"\n");
-	console.log("Password: " + req.body.password +"\n");
-  res.send("Datos de inicio de sesion , validados!");
+	var user = new User({ email: req.body.email, password: req.body.password}); 
+	
+	user.save(function(){
+		res.send("Datos , validados!");
+	});
+	
 });
+
+
 
 app.listen(80);
