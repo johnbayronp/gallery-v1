61b37f971243ce04d79c22021c38667208462951
Johnbayronp <webdeprueba1.1@gmail.com>
2017-12-13 23:01:30 -0500

    Validaciones personalizadas
    Validaciones de las variables del schema

--- app.js
+++ app.js
@@ -35,9 +35,10 @@ app.post("/users" , function(req,res){
 		            }); 
 	
 	console.log(user.password_confirmation);
-
-	user.save(function(){
-		
+	user.save(function(err){
+		if(err){
+			console.log(String(err));
+		}
 		res.send("Datos , validados!");
 	});
 	
--- models/user.js
+++ models/user.js
@@ -3,14 +3,28 @@ var Schema = mongoose.Schema; // Constructor de esquemas
 
 mongoose.connect('mongodb://localhost/fotos'); //conectar la base de datos , localhost/"nombre_de_la_base_de_datos" 
 
+var posibles_valores = ["M" , "F"]; // validaciones
+var email_match = [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,"Coloca un email"]; //expresion regular
+
+var password_validation = { //valida si las contraseñas son iguales
+		validator: function(p){
+       return this.password_confirmation == p;
+   	},
+   	message: "Las contraseñas no son iguales"
+   }
+
+
 var user_schema = new Schema({ // Crea un objeto 
  name:String,
- username: String,
- password:String,
- age: Number,
- email: String,
+ username: {type:String, required: true , maxlength:[50,"Username es muy grande"]},
+ password: {type:String,
+  minlength:[10,"El password es muy corto"],validate:password_validation}, // validaciones para la password
+ age: {type:Number, min:[5, "la edad no puede ser menor que 5"],max:[80,"la edad es muy alta"]}, //validaciones y escribir un mensaje{type:xxx , atributo:[x,"message"]}
+ email: {type:String, required: "el correo es obligatorio", match:email_match}, //validaciones
  date_of_birth: Date, 
+ sex: {type:String, enum:{values:posibles_valores,message:"Opcion no valida"}}, //validaciones
 });
+
 
 user_schema.virtual("password_confirmation").get( function(){
       return this.p_c;
